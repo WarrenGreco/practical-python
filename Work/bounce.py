d8509fae50058bf60636d15229cf634c0221e028
WarrenGreco <102757627+WarrenGreco@users.noreply.github.com>
2022-03-31 11:29:47 -0700

    Update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,15 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100 * 3/5
+bounce = 1
+number = 10
+
+while bounce <= number:
+    print(round(bounce,4), round(height,4))
+    bounce = bounce + 1
+    height = height * 3/5
+
+print('Number of bounces', round(bounce,4))
+print('Final height', round(height,4))
